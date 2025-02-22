6da7e5bb63a39eb3e56af1c97d1ae0920877311c
duncanam <22781288+duncanam@users.noreply.github.com>
2025-02-22 13:34:49 -0800

    Adding quit mapping

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -18,8 +18,11 @@ map("i", "jk", "<ESC>")
 -- Save
 map("n", "<leader>w", "<cmd>w<CR>", { desc = "general save file" })
 
+-- Quit
+map("n", "<leader>q", "<cmd>q<CR>", { desc = "quit"})
+
 -- Explorer
-map("n", "<leader>e", "<cmd>NvimTreeToggle<cr>", { desc = "nvimtree" })
+map("n", "<leader>e", "<cmd>NvimTreeToggle<cr>", { desc = "toggle nvimtree" })
 
 -- whichkey
 map("n", "<leader>kK", "<cmd>WhichKey <CR>", { desc = "whichkey all keymaps" })
